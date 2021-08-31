9eae3eeda142ab9091d3e02676256e4ba15c005a
Abdelghani Adel <a.adel@aladawy.local>
2021-08-31 14:34:28 +0200

    First Edit with JS, Overlay for service

--- js/main.js
+++ js/main.js
@@ -1,36 +1,86 @@
-// Declaring Variables
-let tabs = document.querySelectorAll(".tabs li");           // Get all tabs
-let tabsArray = Array.from(tabs);                           // Put all tabs into array to loop on them later
-let divs = document.querySelectorAll(".content > div");     // Get all divs
-let divsArray = Array.from(divs);                           // Put all divs into array to loop on them later
+let socialIcon = $(".social-icon"),
+    navLink = $('.navbar-light .navbar-nav .nav-item .nav-link'),
+    heroButton = $('.heroWrap button'),
+    serviceCard = $('.ourServices .card');
+
+let prices = $('.pricing .priceWrapper');
+
+// prices.mouseenter((event) => {
+//     event.stopPropagation();
+//     prices.addClass('dimt');
+//     $(event.target).removeClass('dimt')
+//     $(event.target).addClass('scaleup')
+// })
+// prices.mouseleave(() => {
+//     prices.removeClass('dimt')
+//     prices.removeClass('scaleup')
+// })
+
+
+// prices.hover(
+//     (event) => {
+//         event.stopPropagation();
+//         prices.addClass('dimt');
+//         $(event.target).addClass('scaleup');
+//         $(event.target).removeClass('dimt');
+//     } , () => {
+//         prices.removeClass('dimt');
+//         prices.removeClass('scaleup');
+//     }
+// )
+
+
+prices.hover(
+    (event) => {
+        event.stopPropagation();
+        prices.addClass('dimt');
+        $(event.target).addClass('scaleup');
+        $(event.target).removeClass('dimt');
+    } , () => {
+        prices.removeClass('dimt');
+        prices.removeClass('scaleup');
+    }
+)
 
 
 
-// looping on tabs 
-tabsArray.forEach((tab) => {
 
-  // Looping on tabs and adding click event attached to a function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Our Mission Section Tabs
+let tabs = document.querySelectorAll(".tabs li");
+let tabsArray = Array.from(tabs);
+let divs = document.querySelectorAll(".content > div");
+let divsArray = Array.from(divs);
+tabsArray.forEach((tab) => {  
     tab.addEventListener("click", function (e) {
-
-    // Looping on all tabs to remove .active class from them
-    tabsArray.forEach((tab) => {
-        tab.classList.remove("active");
-    });
-
-    // Adding .active class to the current target or this tab we clicked on
-    e.currentTarget.classList.add("active");
-
-    // First, we hide all divs when we click
-    divsArray.forEach((div) => {
-        div.style.display = "none";
-    });
-
-    // Display the div which is attached to our clicked tab with custom HTML attribute
-    document.querySelector(e.currentTarget.dataset.cont).style.display = "block";
+        tabsArray.forEach((tab) => {
+            tab.classList.remove("active");
+        });
+        e.currentTarget.classList.add("active");
+        divsArray.forEach((div) => {
+            div.style.display = "none";
+        });
+        document.querySelector(e.currentTarget.dataset.cont).style.display = "block";
     });
 });
 
 
+
+// Counters
 $('.counter').each(function () {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
@@ -44,17 +94,86 @@ $('.counter').each(function () {
 });
 
 
+// Navigate to to top "button"
 let scrollBtn = document.querySelector(".scroll-btn i");
-
 window.addEventListener("scroll", () => {
     window.pageYOffset > 300
       ? scrollBtn.classList.add("active-btn")
       : scrollBtn.classList.remove("active-btn");
 });
-
 scrollBtn.addEventListener("click", () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
 });
+
+
+
+
+// let ourimages = document.querySelectorAll(".ourServices img")
+// ourimages.forEach(img => {
+//     img.addEventListener('click', (e) => {
+//         let overlay = document.createElement("div");
+//         overlay.className = 'popup-overlay';
+
+//         let popupBox = document.createElement("div");
+//         popupBox.className = 'popup-box';
+//         let popupImage = document.createElement("img");
+//         popupImage.src = img.src
+
+//         document.body.appendChild(overlay);
+//         popupBox.appendChild(popupImage);
+//         overlay.appendChild(popupBox)
+
+//         if (img.alt) {
+//             let imgHeading = document.createElement('h2');
+//             let imgText = document.createTextNode(img.alt);
+//             imgHeading.appendChild(imgText);
+//             popupBox.appendChild(imgHeading)
+//         }
+
+//         let closeButton = document.createElement('span');
+//         let closeButtonText = document.createTextNode("x");
+//         closeButton.appendChild(closeButtonText);
+//         closeButton.className = 'close-button';
+//         popupBox.appendChild(closeButton);
+//     })
+// })
+
+
+let img = $(".ourServices img")
+img.click((event) => {
+
+    let current = event.currentTarget;
+    let imgSource = current.src
+    let title = current.alt
+    let content = `<div class='popup-overlay'>
+                        <div class="popup-box">
+                            <div class='container'>
+                                <div class='row'>
+                                    <div class='col-md-6'>
+                                        <img src= ${imgSource} class='img-thumbnail h-100'>
+                                    </div>
+                                    <div class='col-md-6'>
+                                        <h2 class='my-3 mainColorTXT text-center w-100'>${title}</h2>
+                                        <p class='lead'>The lift & slide system SUPREME S700 represents the new generation of ALUMIL’s high-end systems. It is an excellent solution for projects with demanding requirements, meeting the trend of “transparent” architecture through extensive glass surfaces, with high thermal insulation, safety, and modern design. Its minimal design and its various innovative characteristics, make it the ideal option for projects requiring wide spans for enhanced daylight, outstanding performance, and maximum ease of use.</p>
+                                        <button class='w-100 btn btn-success'>Get Quote</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="close-button">X</span>
+                        </div>
+                    </div>`
+
+    $(document.body).append(content);
+})
+
+
+
+
+document.addEventListener("click", function(e) {
+    if (e.target.className == 'close-button' || e.target.className == 'popup-overlay') {
+        document.querySelector('.popup-overlay').remove();
+    }
+})
